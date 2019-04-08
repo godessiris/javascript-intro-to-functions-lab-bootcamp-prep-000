b43059d93114e193e6094d597a47c6d20a046551
Iris Quintero <godessiris@gmail.com>
2019-04-08 04:22:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
+  var lowercase = "I can't hear you!"
+  lowercase.toLowerCase() === lowercase
+  
   
 }
 
@@ -22,3 +25,12 @@ function sayHiToGrandma(string){
   it('returns "I can\'t hear you!" if `string` is lowercase', function() {
     expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
   })
+  it('returns "YES INDEED!" if `string` is uppercase', function() {
+    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
+  })
+
+  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
+    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
+  })
+})
+
